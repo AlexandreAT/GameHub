587213d7b488b3d2a73204126c57c011985bb420
Alexandre Arribamar <alexandre.arribamar@gmail.com>
2024-02-28 11:21:14 -0300

    Criando algumas APIs

--- Gamehub.Server/Controllers/UsersController.cs
+++ Gamehub.Server/Controllers/UsersController.cs
@@ -20,12 +20,44 @@ namespace Gamehub.Server.Controllers
         [HttpGet]
         public async Task<List<User>> GetUsers() => await _userServices.GetAsync();
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(string id)
+        {
+            var user = await _userServices.GetAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
         [HttpPost]
         public async Task<User> PostUser(User user)
         {
             await _userServices.CreateAsync(user);
 
             return user;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<User>>> UpdateUser(string id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            await _userServices.UpdateAsync(id, user);
+
+            return await _userServices.GetAsync();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<User>>> DeleteUser(string id)
+        {
+            await _userServices.RemoveAsync(id);
+            return await _userServices.GetAsync();
         }
 
     }
