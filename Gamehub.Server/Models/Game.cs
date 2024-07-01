917ba546aef6b45e07c9b0040e153c5b7d8f4770
Alexandre Arribamar <alexandre.arribamar@gmail.com>
2024-07-01 17:53:17 -0300

    Criando o modelo próprio de 'games', para buscar as imagens e os generos dos jogos

--- Gamehub.Server/Models/Game.cs
+++ Gamehub.Server/Models/Game.cs
@@ -1,8 +1,9 @@
 ﻿namespace Gamehub.Server.Models
 {
-    public class Game
+    public class GameModel
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public List<string> Genres { get; set; }
     }
 }
---
+++ Gamehub.Server/Models/GameModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Gamehub.Server.Models
+{
+    public class GameModel
+    {
+        public long? id { get; set; }
+        public string name { get; set; }
+        public List<string> genres { get; set; }
+        public string? imageUrl { get; set; } = null;
+    }
+}
